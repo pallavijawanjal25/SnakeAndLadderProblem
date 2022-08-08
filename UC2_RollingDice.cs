d48a62da920f6c3d619ce0fea0802a2ca0242139
pallavijawanjal25 <109516737+pallavijawanjal25@users.noreply.github.com>
2022-08-09 00:36:12 +0530

    UC2

---
+++ UC2_RollingDice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    static class RollingDice
+    {
+        public static int UC2_DiceNumber()
+        {
+            Console.WriteLine("Rolling Dice..");
+            Random random = new Random();
+            int numberOnDice = random.Next(1, 7);
+            Console.WriteLine("You got " + numberOnDice);
+            return numberOnDice;
+        }
+    }
+}
