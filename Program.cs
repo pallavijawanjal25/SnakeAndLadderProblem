52a9ced1588397893f608ea839af58789ada7626
pallavijawanjal25 <109516737+pallavijawanjal25@users.noreply.github.com>
2022-08-09 00:33:26 +0530

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--***Snake And Ladder***--");
+            SinglePlayer.UC1_SinglePlayer();
+
+            RollingDice.UC2_DiceNumber();
+            PlayerOption.UC3_PlayerMoves();
+            ReapeateTill100.UC4_WinningPosition();
+            CountExact_WinningPosition_Hundred.UC5_WinningPosition();
+            Count_NumberOfTimes_DiceRolled.UC6_CountNumberDice();
+
+            Console.ReadKey();
+        }
+    }
+}
