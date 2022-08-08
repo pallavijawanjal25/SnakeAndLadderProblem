a8c10379d576f490ace8047ac1460bb3d6dcb07b
pallavijawanjal25 <109516737+pallavijawanjal25@users.noreply.github.com>
2022-08-09 00:35:02 +0530

    UC1

---
+++ UC1_SinglePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    static class SinglePlayer
+    {
+        {
+        public static void UC1_SinglePlayer()
+        {
+            int position = 0;
+            Console.WriteLine("Starting Game For Single Player");
+            Console.WriteLine("Starting Position of Player:  " + position);
+            Console.WriteLine("Current Position of Player:  " + position);
+        }
+    }
+}
